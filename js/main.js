c99e07c1681793036a033709712845bb06a64f2b
sungminle <thll12345@naver.com>
2021-09-26 16:35:35 +0900

    Start porject

---
+++ js/main.js
@@ -0,0 +1,164 @@
+// 검색창
+const searchEl = document.querySelector('.search'); 
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function() {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function() {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+// 뱃지
+const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top')
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500){
+    // 뱃지 숨기기
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+    // 상단으로 가는 버튼 보이기
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
+  }else{
+    // 배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+    // 상단으로 가는 버튼 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
+  }
+}, 300));
+  // _.throttl(함수, 시간)
+  // gsap.to(요소, 지속시간, 옵션);
+//상단으로 스크롤 버튼 클릭하면,
+toTopEl.addEventListener('click', function() {
+  //페이지 위치를 최상단으로 부더럽게(.7초 동안)이동
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+})
+
+
+// HEADER 이미지 순차적으로 나타내기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index+1)*.7,
+    opacity: 1
+  } );
+});
+
+// 공지사항 슬라이드
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container',{
+  direction: 'vertical',
+  autoplay: true,
+  loop: true
+});
+
+// 프로모션 이미지 슬라이드
+new Swiper('.promotion .swiper-container', {
+  // dirrction: 'horizontal'  기본값
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10,  // 슬라이드 사이 여백
+  centeredSlides: true,  //1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable: true  // 사용자의 페이지 번호 요소 제어, 가능 여부
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+});
+new Swiper ('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
+
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function(){
+  isHidePromotion = !isHidePromotion
+  if(isHidePromotion) {
+    // 숨김 처리
+    promotionEl.classList.add('hide');
+  } else{
+    // 보임 처리
+    promotionEl.classList.remove('hide');
+  }
+});
+
+/**
+ * 부유하는 요소 관리
+ */
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 '문자 데이터'를,
+  // `parseFloat()`을 통해 소수점을 가지는 '숫자 데이터'로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+// 부유하는(떠 다니는) 요소를 만드는 함수
+function floatingObject(selector, delay, size) {
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+      delay: random(0, delay), // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정.
+      y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
+      repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
+      yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
+      ease: Power1.easeInOut // Easing 함수 적용.
+    }
+  )
+}
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', .5, 15)
+floatingObject('.floating3', 1.5, 20)
+
+// ScrollMagic
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function(spyEl){
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,  //보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 //화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show')  //요소가 화면에 보이면 show클래스 추가
+    .addTo(new ScrollMagic.Controller())  //컨트롤러에 장면을 할당(필수!)
+})
+
+
+// THIS YEAR
+const tihsYear = document.querySelector('.this-year')
+tihsYear.textContent = new Date().getFullYear()
